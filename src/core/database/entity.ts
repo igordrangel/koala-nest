a2701d918464e0af9b4b17139b66c443890a6658
Igor Rangel <igordrangel@gmail.com>
2025-03-19 20:40:29 -0300

    ajuste

--- src/core/database/entity.ts
+++ src/core/database/entity.ts
@@ -1,6 +1,7 @@
 import { AutoMap } from 'automapper-classes'
 import { Optional } from '../@types'
 import { IComparable, IComparableId } from '../utils/interfaces/icomparable'
+import { List } from '../utils/list'
 
 export type EntityProps<T extends IComparable<T>> = Omit<
   Optional<T, 'id'>,
@@ -14,7 +15,13 @@ export abstract class Entity<T extends IComparable<T>>
   id: IComparableId
 
   protected create(props: EntityProps<T>) {
-    Object.assign(this, props)
+    Object.keys(props).forEach((key) => {
+      if (Array.isArray(props[key]) && this[key] instanceof List) {
+        this[key] = new List(props[key])
+      } else {
+        this[key] = props[key]
+      }
+    })
   }
 
   public equals(obj: Entity<T>): boolean {
