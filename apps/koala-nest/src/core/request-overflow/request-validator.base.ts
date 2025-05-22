266acd608a827fa195a5f448a3f34aedce629164
Igor Rangel <igordrangel@gmail.com>
2025-05-22 17:48:32 -0300

    feat: refatora RequestValidatorBase para suportar mapeamento de chaves com '[]'

--- apps/koala-nest/src/core/request-overflow/request-validator.base.ts
+++ apps/koala-nest/src/core/request-overflow/request-validator.base.ts
@@ -1,9 +1,25 @@
 import { ZodType } from 'zod'
 
-export abstract class RequestValidatorBase<TRequest> {
-  constructor(private _request: TRequest) {}
+export abstract class RequestValidatorBase<
+  TRequest extends Record<string, any>,
+> {
+  private _request: Record<string, any>
+
+  constructor(request: TRequest) {
+    this._request = { ...request }
+  }
 
   validate(): TRequest {
+    Object.keys(this._request).forEach((key) => {
+      if (key.includes('[]')) {
+        const newKey = key.replace('[]', '')
+        const value = this._request[key]
+        this._request[newKey] =
+          typeof value === 'string' ? value.split(',') : value
+        delete this._request[key]
+      }
+    })
+
     const requestParsed = this.schema.safeParse(this._request)
 
     if (requestParsed.success) {
