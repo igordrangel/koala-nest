6d6aab987fb2ad9c0abe152f016e07fad0e9a56b
Igor Rangel <igordrangel@gmail.com>
2025-06-12 12:01:16 -0300

    feat: implementa função para aplicar máscara em números de documentos

---
+++ apps/koala-nest/src/core/controllers/schemas/set-mask-document-number.schema.ts
@@ -0,0 +1,13 @@
+import { maskCnpj, maskCpf } from '@koalarx/utils'
+
+export function setMaskDocumentNumber(document?: string) {
+  if (!document) {
+    return ''
+  }
+
+  const documentWithoutMask = document.replace(/[^0-9]/g, '')
+
+  return documentWithoutMask.length === 11
+    ? maskCpf(documentWithoutMask)
+    : maskCnpj(documentWithoutMask)
+}
