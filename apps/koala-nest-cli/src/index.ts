544f64790562db93d6a3a2fb1580fdd9d94463e0
Igor Rangel <igor.rangel_meta@prestador.globo>
2026-01-19 18:32:12 -0300

    fix: corrigir caminho do package.json no CLI e adicionar ajustes de versão

--- apps/koala-nest-cli/src/index.ts
+++ apps/koala-nest-cli/src/index.ts
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const packageJson = JSON.parse(
-  readFileSync(join(__dirname, '../package.json'), 'utf-8')
+  readFileSync(join(__dirname, 'package.json'), 'utf-8')
 )
 
 const banner = `
